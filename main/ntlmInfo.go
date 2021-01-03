1771dd4762c1f9cb123d6b46348c95b1c2120002
bogey3 <sam@sbogart.com>
2021-01-03 16:58:58 -0500

    Added os specific files for retrieving HTTP proxy configuration. Added readme.md

--- main/ntlmInfo.go
+++ main/ntlmInfo.go
@@ -13,13 +13,13 @@ import (
 	"time"
 )
 
-func getAuthType(authUrl string) string{
+func getAuthType(authUrl string) string {
 	resp, _ := http.Get(authUrl)
-	if resp.StatusCode == 401{
+	if resp.StatusCode == 401 {
 		authHeader, ok := resp.Header["Www-Authenticate"]
-		if ok{
-			for _, header := range authHeader{
-				if header == "Negotiate" || header == "NTLM"{
+		if ok {
+			for _, header := range authHeader {
+				if header == "Negotiate" || header == "NTLM" {
 					return header
 				}
 			}
@@ -28,16 +28,25 @@ func getAuthType(authUrl string) string{
 	return ""
 }
 
-func pad(inputArray []byte, length int) []byte{
+func pad(inputArray []byte, length int) []byte {
 	out := make([]byte, len(inputArray))
 	copy(out, inputArray)
-	for i:= len(inputArray); i<length; i++{
+	for i := len(inputArray); i < length; i++ {
 		out = append(out, byte(0))
 	}
 	return out
 }
 
-func httpType2(ntlmUrl string) []byte{
+func setHTTPClientDefaults() {
+	//if a proxy is in use, set the proxy in the default transport, also set HTTPS verify to false
+	proxy, err := getProxy()
+	if err == nil {
+		http.DefaultTransport.(*http.Transport).Proxy = http.ProxyURL(&proxy)
+	}
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+}
+
+func httpType2(ntlmUrl string) []byte {
 
 	authType := getAuthType(ntlmUrl)
 	if strings.ToLower(authType) == "ntlm" || strings.ToLower(authType) == "negotiate" {
@@ -55,16 +64,16 @@ func httpType2(ntlmUrl string) []byte{
 		}
 		type2ChallengeBytes, _ := base64.StdEncoding.DecodeString(type2Challenge)
 		return type2ChallengeBytes
-	}else{
+	} else {
 		fmt.Println("This url does not support NTLM or Negotiate authentication.")
 	}
 	return nil
 }
 
-func rdpType2(ntlmHost string, port string) []byte{
+func rdpType2(ntlmHost string, port string) []byte {
 	challenge := make([]byte, 2048)
-	d := net.Dialer{Timeout:10 * time.Second}
-	conn, err := tls.DialWithDialer(&d,"tcp", ntlmHost + ":" + port, &tls.Config{InsecureSkipVerify: true})
+	d := net.Dialer{Timeout: 10 * time.Second}
+	conn, err := tls.DialWithDialer(&d, "tcp", ntlmHost+":"+port, &tls.Config{InsecureSkipVerify: true})
 	if err == nil {
 		NLAData := append(append([]byte{48, 55, 160, 3, 2, 1, 96, 161, 48, 48, 46, 48, 44, 160, 42, 4, 40}, requestForChallenge...), []byte{0, 0, 10, 0, 99, 69, 0, 0, 0, 15}...)
 		conn.Write(NLAData)
@@ -76,17 +85,16 @@ func rdpType2(ntlmHost string, port string) []byte{
 	return nil
 }
 
-
-func smtpType2(ntlmHost string, port string) []byte{
+func smtpType2(ntlmHost string, port string) []byte {
 	buffer := make([]byte, 1024)
-	d := net.Dialer{Timeout:10 * time.Second}
-	conn, err := d.Dial("tcp", ntlmHost + ":" + port)
-	if err == nil{
+	d := net.Dialer{Timeout: 10 * time.Second}
+	conn, err := d.Dial("tcp", ntlmHost+":"+port)
+	if err == nil {
 		conn.Read(buffer)
 		conn.Write([]byte("EHLO test.com\r\n"))
 		n, _ := conn.Read(buffer)
 		data := string(buffer[:n])
-		if strings.Contains(data, "NTLM"){
+		if strings.Contains(data, "NTLM") {
 			conn.Write([]byte("AUTH NTLM " + requstForChallengeString + "\r\n"))
 			n, _ = conn.Read(buffer)
 			data = string(buffer[:n])
@@ -94,22 +102,21 @@ func smtpType2(ntlmHost string, port string) []byte{
 			type2ChallengeBytes, _ := base64.StdEncoding.DecodeString(challengeStr)
 			return type2ChallengeBytes
 
-		}else{
+		} else {
 			fmt.Println("This SMTP server does not support NTLM authentication.")
 		}
-	}else{
+	} else {
 		fmt.Println("Could not connect to SMTP server")
 	}
 	return nil
 }
 
-
-func decodeChallenge(challenge []byte, offset uint16) [6][2]string{
+func decodeChallenge(challenge []byte, offset uint16) [6][2]string {
 	data := challenge[offset:]
 	types := [6]string{"Server Name:    ", "Domain Name:    ", "Server FQDN:    ", "Domain FQDN:    ", "Parent Domain:  ", "OS Version:     "}
 	dataOut := [6][2]string{}
-	for i := 0; i<5; i++{
-		index := binary.LittleEndian.Uint16(pad(data[0:2], 4))-1
+	for i := 0; i < 5; i++ {
+		index := binary.LittleEndian.Uint16(pad(data[0:2], 4)) - 1
 		if index != 6 {
 			dataType := types[index]
 			dataLength := binary.LittleEndian.Uint16(pad(data[2:4], 4)) + 4
@@ -121,44 +128,43 @@ func decodeChallenge(challenge []byte, offset uint16) [6][2]string{
 		}
 	}
 
-	if offset > 48{
+	if offset > 48 {
 		dataOut[5][0] = types[5]
 		major := int(challenge[48])
 		minor := int(challenge[49])
 		build := int(binary.LittleEndian.Uint16(pad(challenge[50:52], 4)))
 		majorMinor := ""
-		switch strconv.Itoa(major) + "." + strconv.Itoa(minor){
+		switch strconv.Itoa(major) + "." + strconv.Itoa(minor) {
 		case "5.0":
-			majorMinor = "Windows 2000 (Build " + strconv.Itoa(build) +")"
+			majorMinor = "Windows 2000 (Build " + strconv.Itoa(build) + ")"
 		case "5.1":
-			majorMinor = "Windows XP/Server 2003 (R2) (Build " + strconv.Itoa(build) +")"
+			majorMinor = "Windows XP/Server 2003 (R2) (Build " + strconv.Itoa(build) + ")"
 		case "5.2":
-			majorMinor = "Windows XP/Server 2003 (R2) (Build " + strconv.Itoa(build) +")"
+			majorMinor = "Windows XP/Server 2003 (R2) (Build " + strconv.Itoa(build) + ")"
 		case "6.0":
-			majorMinor = "Windows Vista/Server 2008 (Build " + strconv.Itoa(build) +")"
+			majorMinor = "Windows Vista/Server 2008 (Build " + strconv.Itoa(build) + ")"
 		case "6.1":
-			majorMinor = "Windows 7/Server 2008 R2 (Build " + strconv.Itoa(build) +")"
+			majorMinor = "Windows 7/Server 2008 R2 (Build " + strconv.Itoa(build) + ")"
 		case "6.2":
-			majorMinor = "Windows 8/Server 2012 (Build " + strconv.Itoa(build) +")"
+			majorMinor = "Windows 8/Server 2012 (Build " + strconv.Itoa(build) + ")"
 		case "6.3":
-			majorMinor = "Windows 8.1/Server 2012 R2 (Build " + strconv.Itoa(build) +")"
+			majorMinor = "Windows 8.1/Server 2012 R2 (Build " + strconv.Itoa(build) + ")"
 		case "10.0":
 			if build >= 17623 {
-				majorMinor = "Windows 10/Server 2019 (Build " + strconv.Itoa(build) +")"
-			}else{
-				majorMinor = "Windows 10/Server 2016 (Build " + strconv.Itoa(build) +")"
+				majorMinor = "Windows 10/Server 2019 (Build " + strconv.Itoa(build) + ")"
+			} else {
+				majorMinor = "Windows 10/Server 2016 (Build " + strconv.Itoa(build) + ")"
 			}
 		default:
 			majorMinor = strconv.Itoa(major) + "." + strconv.Itoa(minor) + "." + strconv.Itoa(build)
 		}
 		dataOut[5][1] = majorMinor
 
-
 	}
 	return dataOut
 }
 
-func usage(){
+func usage() {
 	fmt.Println("NTLM Authentication Information Disclosure\n\n\t" + os.Args[0] + " https://mail.example.com/ews\n\t" + os.Args[0] + " smtp://mail.example.com\n\t" + os.Args[0] + " rdp://computer.example.com")
 	os.Exit(1)
 }
@@ -166,9 +172,9 @@ func usage(){
 var requestForChallenge = []byte{78, 84, 76, 77, 83, 83, 80, 0, 1, 0, 0, 0, 7, 130, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 var requstForChallengeString = "TlRMTVNTUAABAAAAB4IIAAAAAAAAAAAAAAAAAAAAAAA="
 
-func main()  {
+func main() {
 	//Test for correct number of command line arguments, and presence of "://" in argument, otherwise print help
-	if len(os.Args) != 2 || !strings.Contains(os.Args[1], "://"){
+	if len(os.Args) != 2 || !strings.Contains(os.Args[1], "://") {
 		usage()
 	}
 
@@ -179,20 +185,20 @@ func main()  {
 	//Switch statement to use the correct function to retrieve the NTLM challenge based on the protocol provided
 	switch protocol {
 	case "http":
+		setHTTPClientDefaults()
 		challenge = httpType2(targetUrl)
 	case "https":
-		//Allow the use of invalid HTTPS certificates
-		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		setHTTPClientDefaults()
 		challenge = httpType2(targetUrl)
 	case "rdp":
 		urlAndPort := strings.Split(targetUrl, ":")
-		if len(urlAndPort) == 2{
+		if len(urlAndPort) == 2 {
 			urlAndPort = append(urlAndPort, "3389")
 		}
 		challenge = rdpType2(strings.Replace(urlAndPort[1], "//", "", 1), urlAndPort[2])
 	case "smtp":
 		urlAndPort := strings.Split(targetUrl, ":")
-		if len(urlAndPort) == 2{
+		if len(urlAndPort) == 2 {
 			urlAndPort = append(urlAndPort, "25")
 		}
 		challenge = smtpType2(strings.Replace(urlAndPort[1], "//", "", 1), urlAndPort[2])
@@ -200,11 +206,11 @@ func main()  {
 		usage()
 	}
 
-	if challenge != nil{
+	if challenge != nil {
 		offset := binary.LittleEndian.Uint16(challenge[44:48])
 		data := decodeChallenge(challenge, offset)
 		for _, value := range data {
-			if value[0] + value[1] != "" {
+			if value[0]+value[1] != "" {
 				fmt.Println(value[0] + value[1])
 			}
 		}
---
+++ readme.md
@@ -0,0 +1,43 @@
+# NTLMSSP Information Disclosure
+
+This program was written using Go version 1.15.5, other versions will likely work but are not tested.
+
+This program can be used to extract information using the NTLMSSP challenge provided during NTLM authentication.
+
+## How to compile
+Depending on your host OS compile instruction may vary slightly, however they should be quite similar.
+
+You can compile this with a simple `go build /path/to/main/directory` on Linux or Windows.
+
+If your current directory when running the command is the main directory, you can simply run `go build .`.
+
+
+## How to use this program
+Once compiled this software will take one argument as the target URL.
+
+Below are a few examples on how to run this software:
+```
+ntlmInfo https://mail.domain.com/ews
+ntlmInfo smtp://mail.domain.com
+ntlmInfo smtp://mail.domain.com:2525
+ntlmInfo rdp://192.168.0.10
+ntlmInfo rdp://192.168.0.10:4489
+```
+
+If a port is not specified the default will be used as follows:
+```
+RDP:   3389
+SMTP:  25
+HTTP:  80
+HTTPS: 443
+```
+
+The output should be as follows:
+```
+Domain Name:    SUBDOMAIN
+Server Name:    HOSTNETBIOS
+Domain FQDN:    subdomain.netbiosname.tld
+Server FQDN:    hostnetbios.subdomain.netbiosname.tld
+Parent Domain:  netbiosname.tld
+OS Version:     Windows 10/Server 2019 (Build 19041)
+```
