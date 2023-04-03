088d140c5d083a48fed1ed25950c74d8caef3ea9
bogey3 <29926385+bogey3@users.noreply.github.com>
2023-04-03 17:00:19 -0400

    Update ntlmInfo.go

--- ntlmInfo.go
+++ ntlmInfo.go
@@ -24,25 +24,25 @@ const REQ_FOR_CHALLENGE = "TlRMTVNTUAABAAAAB4IIAAAAAAAAAAAAAAAAAAAAAAA="
 var REQ_FOR_CHALLENGE_BYTES = []byte{78, 84, 76, 77, 83, 83, 80, 0, 1, 0, 0, 0, 7, 130, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 type TargetStruct struct {
-	targetURL *url.URL
-	challenge type2ChallengeStruct
+	TargetURL *url.URL
+	Challenge type2ChallengeStruct
 }
 
 type type2ChallengeStruct struct {
-	rawChallenge    []byte
-	serverName      string
-	domainName      string
-	serverFQDN      string
-	domainFQDN      string
-	parentDomain    string
-	osVersionNumber string
-	osVersionString string
+	RawChallenge []byte
+	ServerName   string
+	DomainName string
+	ServerFQDN   string
+	DomainFQDN      string
+	ParentDomain    string
+	OsVersionNumber string
+	OsVersionString string
 }
 
 func (t *TargetStruct) GetChallenge() error {
 	var err error
 
-	switch t.targetURL.Scheme {
+	switch t.TargetURL.Scheme {
 	case "http":
 		err = t.getHTTPChallenge()
 	case "https":
@@ -56,7 +56,7 @@ func (t *TargetStruct) GetChallenge() error {
 	}
 
 	if err == nil {
-		t.challenge.decode()
+		t.Challenge.decode()
 
 	}
 	return err
@@ -68,7 +68,7 @@ func (t *TargetStruct) getHTTPChallenge() error {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
 	wwwAuthHeader := "NTLM " + REQ_FOR_CHALLENGE
-	type1Request, err := http.NewRequest("GET", t.targetURL.String(), nil)
+	type1Request, err := http.NewRequest("GET", t.TargetURL.String(), nil)
 	if err == nil {
 		type1Request.Header.Add("Authorization", wwwAuthHeader)
 		type2Response, err := http.DefaultClient.Do(type1Request)
@@ -82,7 +82,7 @@ func (t *TargetStruct) getHTTPChallenge() error {
 			if strings.Contains(type2Challenge, ",") {
 				type2Challenge = type2Challenge[:strings.LastIndex(type2Challenge, ",")]
 			}
-			t.challenge.rawChallenge, err = base64.StdEncoding.DecodeString(type2Challenge)
+			t.Challenge.RawChallenge, err = base64.StdEncoding.DecodeString(type2Challenge)
 			return err
 		}
 	}
@@ -91,14 +91,14 @@ func (t *TargetStruct) getHTTPChallenge() error {
 
 func (t *TargetStruct) getRDPChallenge() error {
 
-	if !strings.Contains(t.targetURL.Host, ":") {
-		t.targetURL.Host = t.targetURL.Host + ":3389"
+	if !strings.Contains(t.TargetURL.Host, ":") {
+		t.TargetURL.Host = t.TargetURL.Host + ":3389"
 	}
 	challenge := make([]byte, 2048)
 	var pConn net.Conn
 	var err error
 	d := net.Dialer{Timeout: 10 * time.Second}
-	pConn, err = d.Dial("tcp", t.targetURL.Host)
+	pConn, err = d.Dial("tcp", t.TargetURL.Host)
 	conn := tls.Client(pConn, &tls.Config{InsecureSkipVerify: true})
 
 	if err == nil {
@@ -108,7 +108,7 @@ func (t *TargetStruct) getRDPChallenge() error {
 			readLen, err := conn.Read(challenge)
 			if err == nil {
 				challenge = challenge[23:readLen]
-				t.challenge.rawChallenge = challenge
+				t.Challenge.RawChallenge = challenge
 			}
 		}
 	}
@@ -116,14 +116,14 @@ func (t *TargetStruct) getRDPChallenge() error {
 }
 
 func (t *TargetStruct) getSMTPChallenge() error {
-	if !strings.Contains(t.targetURL.Host, ":") {
-		t.targetURL.Host = t.targetURL.Host + ":25"
+	if !strings.Contains(t.TargetURL.Host, ":") {
+		t.TargetURL.Host = t.TargetURL.Host + ":25"
 	}
 	buffer := make([]byte, 1024)
 	var conn net.Conn
 	var err error
 	d := net.Dialer{Timeout: 10 * time.Second}
-	conn, err = d.Dial("tcp", t.targetURL.Host)
+	conn, err = d.Dial("tcp", t.TargetURL.Host)
 
 	if err == nil {
 		conn.Read(buffer)
@@ -138,7 +138,7 @@ func (t *TargetStruct) getSMTPChallenge() error {
 				challengeStr := strings.Split(data, " ")[1]
 				type2ChallengeBytes, err := base64.StdEncoding.DecodeString(challengeStr)
 				if err == nil {
-					t.challenge.rawChallenge = type2ChallengeBytes
+					t.Challenge.RawChallenge = type2ChallengeBytes
 				}
 			}
 		} else {
@@ -149,73 +149,73 @@ func (t *TargetStruct) getSMTPChallenge() error {
 }
 
 func(t *TargetStruct) Print(){
-	if t.challenge.rawChallenge != nil {
+	if t.Challenge.RawChallenge != nil {
 		fmt.Printf("+%s+%s+\n", strings.Repeat("-", 19), strings.Repeat("-", 47))
-		fmt.Printf("| %17s | %-45s |\n", "Server Name", t.challenge.serverName)
-		fmt.Printf("| %17s | %-45s |\n", "Domain Name", t.challenge.domainName)
-		fmt.Printf("| %17s | %-45s |\n", "Server FQDN", t.challenge.serverFQDN)
-		fmt.Printf("| %17s | %-45s |\n", "Domain FQDN", t.challenge.domainFQDN)
-		fmt.Printf("| %17s | %-45s |\n", "Parent Domain", t.challenge.parentDomain)
-		fmt.Printf("| %17s | %-45s |\n", "OS Version Number", t.challenge.osVersionNumber)
-		fmt.Printf("| %17s | %-45s |\n", "OS Version", t.challenge.osVersionString)
+		fmt.Printf("| %17s | %-45s |\n", "Server Name", t.Challenge.ServerName)
+		fmt.Printf("| %17s | %-45s |\n", "Domain Name", t.Challenge.DomainName)
+		fmt.Printf("| %17s | %-45s |\n", "Server FQDN", t.Challenge.ServerFQDN)
+		fmt.Printf("| %17s | %-45s |\n", "Domain FQDN", t.Challenge.DomainFQDN)
+		fmt.Printf("| %17s | %-45s |\n", "Parent Domain", t.Challenge.ParentDomain)
+		fmt.Printf("| %17s | %-45s |\n", "OS Version Number", t.Challenge.OsVersionNumber)
+		fmt.Printf("| %17s | %-45s |\n", "OS Version", t.Challenge.OsVersionString)
 		fmt.Printf("+%s+%s+\n", strings.Repeat("-", 19), strings.Repeat("-", 47))
 	}
 }
 
 func (t *type2ChallengeStruct) decode() {
-	offset := binary.LittleEndian.Uint16(t.rawChallenge[44:48])
-	data := t.rawChallenge[offset:]
+	offset := binary.LittleEndian.Uint16(t.RawChallenge[44:48])
+	data := t.RawChallenge[offset:]
 	for i := 0; i < 5; i++ {
 		dataType := binary.LittleEndian.Uint16(data[0:2])
 		dataLength := binary.LittleEndian.Uint16(data[2:4]) + 4
 		text := strings.Replace(string(data[4:dataLength]), "\x00", "", -1)
 		switch dataType {
 		case SERVER_NAME:
-			t.serverName = text
+			t.ServerName = text
 		case DOMAIN_NAME:
-			t.domainName = text
+			t.DomainName = text
 		case SERVER_FQDN:
-			t.serverFQDN = text
+			t.ServerFQDN = text
 		case DOMAIN_FQDN:
-			t.domainFQDN = text
+			t.DomainFQDN = text
 		case PARENT_DOMAIN:
-			t.parentDomain = text
+			t.ParentDomain = text
 		}
 		data = data[dataLength:]
 	}
 
 	if offset > 48 {
-		major := int(t.rawChallenge[48])
-		minor := int(t.rawChallenge[49])
-		build := int(binary.LittleEndian.Uint16(t.rawChallenge[50:52]))
-		t.osVersionNumber = strconv.Itoa(major) + "." + strconv.Itoa(minor) + "." + strconv.Itoa(build)
+		major := int(t.RawChallenge[48])
+		minor := int(t.RawChallenge[49])
+		build := int(binary.LittleEndian.Uint16(t.RawChallenge[50:52]))
+		t.OsVersionNumber = strconv.Itoa(major) + "." + strconv.Itoa(minor) + "." + strconv.Itoa(build)
 		switch strconv.Itoa(major) + "." + strconv.Itoa(minor) {
 		case "5.0":
-			t.osVersionString = "Windows 2000 (Build " + strconv.Itoa(build) + ")"
+			t.OsVersionString = "Windows 2000 (Build " + strconv.Itoa(build) + ")"
 		case "5.1":
-			t.osVersionString = "Windows XP/Server 2003 (R2) (Build " + strconv.Itoa(build) + ")"
+			t.OsVersionString = "Windows XP/Server 2003 (R2) (Build " + strconv.Itoa(build) + ")"
 		case "5.2":
-			t.osVersionString = "Windows XP/Server 2003 (R2) (Build " + strconv.Itoa(build) + ")"
+			t.OsVersionString = "Windows XP/Server 2003 (R2) (Build " + strconv.Itoa(build) + ")"
 		case "6.0":
-			t.osVersionString = "Windows Vista/Server 2008 (Build " + strconv.Itoa(build) + ")"
+			t.OsVersionString = "Windows Vista/Server 2008 (Build " + strconv.Itoa(build) + ")"
 		case "6.1":
-			t.osVersionString = "Windows 7/Server 2008 R2 (Build " + strconv.Itoa(build) + ")"
+			t.OsVersionString = "Windows 7/Server 2008 R2 (Build " + strconv.Itoa(build) + ")"
 		case "6.2":
-			t.osVersionString = "Windows 8/Server 2012 (Build " + strconv.Itoa(build) + ")"
+			t.OsVersionString = "Windows 8/Server 2012 (Build " + strconv.Itoa(build) + ")"
 		case "6.3":
-			t.osVersionString = "Windows 8.1/Server 2012 R2 (Build " + strconv.Itoa(build) + ")"
+			t.OsVersionString = "Windows 8.1/Server 2012 R2 (Build " + strconv.Itoa(build) + ")"
 		case "10.0":
 			if build >= 22000 {
-				t.osVersionString = "Windows 11/Server 2022 (Build " + strconv.Itoa(build) + ")"
+				t.OsVersionString = "Windows 11/Server 2022 (Build " + strconv.Itoa(build) + ")"
 			} else if build >= 20348 {
-				t.osVersionString = "Windows 10/Server 2022 (Build " + strconv.Itoa(build) + ")"
+				t.OsVersionString = "Windows 10/Server 2022 (Build " + strconv.Itoa(build) + ")"
 			} else if build >= 17623 {
-				t.osVersionString = "Windows 10/Server 2019 (Build " + strconv.Itoa(build) + ")"
+				t.OsVersionString = "Windows 10/Server 2019 (Build " + strconv.Itoa(build) + ")"
 			} else {
-				t.osVersionString = "Windows 10/Server 2016 (Build " + strconv.Itoa(build) + ")"
+				t.OsVersionString = "Windows 10/Server 2016 (Build " + strconv.Itoa(build) + ")"
 			}
 		default:
-			t.osVersionString = strconv.Itoa(major) + "." + strconv.Itoa(minor) + "." + strconv.Itoa(build)
+			t.OsVersionString = strconv.Itoa(major) + "." + strconv.Itoa(minor) + "." + strconv.Itoa(build)
 		}
 	}
 }
